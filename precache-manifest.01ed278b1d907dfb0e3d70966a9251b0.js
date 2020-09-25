76ede824964cf292db758ea8667f0a29c2fd863b
Arunava Ganguly <69519601+ArunavaGanguly@users.noreply.github.com>
2020-09-26 02:10:52 +0530

    Updates

---
+++ precache-manifest.01ed278b1d907dfb0e3d70966a9251b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db080821e28b1a66a62bbf690371e65c",
+    "url": "/techsolutions/index.html"
+  },
+  {
+    "revision": "c8d7652f286c15aaa60f",
+    "url": "/techsolutions/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5c6f4a08efc2948a57b0",
+    "url": "/techsolutions/static/css/main.3fa03396.chunk.css"
+  },
+  {
+    "revision": "c8d7652f286c15aaa60f",
+    "url": "/techsolutions/static/js/2.6a83155a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/techsolutions/static/js/2.6a83155a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6f4a08efc2948a57b0",
+    "url": "/techsolutions/static/js/main.64e32bbd.chunk.js"
+  },
+  {
+    "revision": "a99c5be4014ea71f0826",
+    "url": "/techsolutions/static/js/runtime-main.408ec55c.js"
+  },
+  {
+    "revision": "08fa9a7d9edc3d6da8c9ca47fb8e987d",
+    "url": "/techsolutions/static/media/finance.08fa9a7d.png"
+  },
+  {
+    "revision": "2036bfaccd7338a8bd883185b2e31e15",
+    "url": "/techsolutions/static/media/grow.2036bfac.png"
+  },
+  {
+    "revision": "fc5ab06ece6657f0cc82345c9b036d48",
+    "url": "/techsolutions/static/media/mobile.fc5ab06e.png"
+  },
+  {
+    "revision": "a969c30eb50166033a5414e7693c5779",
+    "url": "/techsolutions/static/media/tech3.a969c30e.jpg"
+  },
+  {
+    "revision": "e2414742225fb80dc3bc7db151c4d7fa",
+    "url": "/techsolutions/static/media/tech4.e2414742.png"
+  },
+  {
+    "revision": "bd3c048458b07e923e7c2ab94feaae31",
+    "url": "/techsolutions/static/media/tech5.bd3c0484.png"
+  },
+  {
+    "revision": "bd09d882c3cdafab028902564059b64b",
+    "url": "/techsolutions/static/media/tech6.bd09d882.jpg"
+  },
+  {
+    "revision": "3d9cec5b380e8de0021e459a068f7c67",
+    "url": "/techsolutions/static/media/tech8.3d9cec5b.jpg"
+  }
+]);
